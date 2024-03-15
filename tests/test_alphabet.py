03ccf39aa1490600d9cf0865d2f13b9a82eb0901
masha-mtc <107871776+masha-mtc@users.noreply.github.com>
2024-03-15 15:42:22 +0600

    initial implementation

---
+++ tests/test_alphabet.py
@@ -0,0 +1,11 @@
+from foapy.alphabet import alphabet
+
+
+def test_alphabet():
+    """Test list of unique array elements"""
+    # ----test_1----
+    assert alphabet(["a", "c", "c", "e", "d", "a"]) == ["a", "c", "e", "d"]
+    # ----test_2----
+    assert alphabet([0, 1, 2, 3, 4]) == [0, 1, 2, 3, 4]
+    # ----test_3----
+    assert alphabet([]) == []
