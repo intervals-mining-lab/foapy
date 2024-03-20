b43d248baaa0a713d4ea5afda413e9a8ddf52c73
masha-mtc <107871776+masha-mtc@users.noreply.github.com>
2024-03-20 20:56:04 +0600

    correction

--- tests/test_alphabet.py
+++ tests/test_alphabet.py
@@ -2,9 +2,14 @@ from foapy.alphabet import alphabet
 
 """Test list of unique array elements"""
 
+
 def test_string_values():
-    assert (alphabet(['a', 'c', 'c', 'e', 'd', 'a']) == ['a', 'c', 'e', 'd'])
+    assert alphabet(["a", "c", "c", "e", "d", "a"]) == ["a", "c", "e", "d"]
+
+
 def test_int_values():
-    assert (alphabet([0, 1, 2, 3, 4]) == [0, 1, 2, 3, 4])
+    assert alphabet([0, 1, 2, 3, 4]) == [0, 1, 2, 3, 4]
+
+
 def test_void():
-    assert (alphabet([]) == [])
+    assert alphabet([]) == []
