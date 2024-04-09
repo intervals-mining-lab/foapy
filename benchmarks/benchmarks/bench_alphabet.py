72fc165baeda7740bc971af4f3668983890a6224
Igor Rodionov <goruha@gmail.com>
2024-04-09 04:06:45 +0200

    Run only quick benchmarks for PRs by default

--- benchmarks/benchmarks/bench_alphabet.py
+++ benchmarks/benchmarks/bench_alphabet.py
@@ -7,7 +7,7 @@ from numpy import fix
 from foapy.alphabet import alphabet
 
 length = [5, 50, 500, 5000, 50000, 500000, 5000000, 50000000]
-skip_args = [
+skip = [
     (5000000, "Worst"),
     (5000000, "DNA"),
     (5000000, "Normal"),
@@ -53,14 +53,14 @@ class AlphabetSuite:
         elif case == "Worst":
             self.data = worst_case(length)
 
-    @skip_params_if(skip_args, os.getenv("QUICK_BENCHMARK") == "true")
+    @skip_params_if(skip, os.getenv("QUICK_BENCHMARK") == "true")
     def time_alphabet(self, length, case):
         alphabet(self.data)
 
-    @skip_params_if(skip_args, os.getenv("QUICK_BENCHMARK") == "true")
+    @skip_params_if(skip, os.getenv("QUICK_BENCHMARK") == "true")
     def mem_alphabet(self, length, case):
         return alphabet(self.data)
 
-    @skip_params_if(skip_args, os.getenv("QUICK_BENCHMARK") == "true")
+    @skip_params_if(skip, os.getenv("QUICK_BENCHMARK") == "true")
     def peakmem_alphabet(self, length, case):
         return alphabet(self.data)
