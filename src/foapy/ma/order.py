b3f7b01cc47aab04a565e2fb489f6293790de9be
Maximus2012 <mkochetkov9@gmail.com>
2024-04-27 13:46:40 +0600

    stayed only ma_alphabet

--- src/foapy/ma/order.py
+++ src/foapy/ma/order.py
@@ -1,8 +1,4 @@
 import numpy as np
-import numpy.ma as ma
-
-from foapy.exceptions import Not1DArrayException
-from foapy.ma.alphabet import alphabet
 
 
 def order(X, return_alphabet=False) -> np.ma.MaskedArray:
@@ -109,27 +105,4 @@ def order(X, return_alphabet=False) -> np.ma.MaskedArray:
     >>> b
     Exception
     """
-    if X.ndim > 1:  # Checking for d1 array
-        raise Not1DArrayException(
-            {"message": f"Incorrect array form. Expected d1 array, exists {X.ndim}"}
-        )
-
-    alphabet_values = alphabet(X)
-
-    result = np.empty((len(alphabet_values), (len(X))), np.int64)
-    mask = np.ones((len(alphabet_values), (len(X))), np.int64)
-
-    alphabet_seq = {}
-    counter = 0
-    for i in ma.getdata(alphabet_values):
-        alphabet_seq[counter] = i
-        counter += 1
-    for idx_row, i in alphabet_seq.items():  # getting array sequence
-        for idx_col, j in enumerate(ma.getdata(X)):
-            if i == j:
-                result[idx_row][idx_col] = idx_row
-                mask[idx_row][idx_col] = 0
-
-    if return_alphabet:  # Checking for get alhabet (optional)
-        return ma.masked_array(result, mask=mask), alphabet_values
-    return ma.masked_array(result, mask=mask)
+    pass
