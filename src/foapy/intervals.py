3cdab3256ffb4b55dc2f1ef11b892b7f527bffad
Igor Rodionov <goruha@gmail.com>
2024-07-13 23:54:50 +0200

    Improve intervals

--- src/foapy/intervals.py
+++ src/foapy/intervals.py
@@ -1,43 +1,60 @@
+import numpy as np
+from numpy import nonzero
+
+
 def intervals(X, binding, mode):
     """
     Finding array of array of intervals of the
     uniform  sequences in the given input sequence
     """
-    order_list = X
-    result = []
-    print(order_list)
-    counter = 0
+    convert_arr = np.asanyarray(X)
+    shape = convert_arr.shape
+
+    if mode == 4:
+        shape = shape + (2,)
+
+    result = np.zeros(shape=shape, dtype=int)
+
     first_elements = {}
     position_elem = {}
+
     # Reverse the sequence if binding is to the end
     if binding == 2:
-        X = X[::-1]
+        convert_arr = convert_arr[::-1]
 
-    for idx, elem in enumerate(X):
+    for idx, elem in enumerate(convert_arr):
         if elem not in first_elements:
             first_elements[elem] = idx
-            position_elem[elem] = idx
         else:
             interval = idx - position_elem[elem]
-            result.append(interval)
-            position_elem[elem] = idx
+            if mode == 4:
+                result[idx][0] = interval
+            else:
+                result[idx] = interval
+        position_elem[elem] = idx
+
+    if mode == 1:
+        result = result[nonzero(result)]
 
     if mode == 2:
         for elem in first_elements:
-            result.insert(first_elements[elem], first_elements[elem] + 1)
+            result[first_elements[elem]] = first_elements[elem] + 1
 
     elif mode == 3:
         for elem in first_elements:
-            result.insert(
-                first_elements[elem],
-                len(X) - position_elem[elem] + first_elements[elem],
+            result[first_elements[elem]] = (
+                len(X) - position_elem[elem] + first_elements[elem]
             )
 
     elif mode == 4:
         for elem in first_elements:
-            result.insert(first_elements[elem], first_elements[elem] + 1)
-        for i, counter in enumerate(sorted(position_elem.values())):
-            result.insert(counter + i + 1, len(X) - counter)
+            result[first_elements[elem]][0] = first_elements[elem] + 1
+        for elem in position_elem:
+            counter = position_elem[elem]
+            result[counter][1] = len(X) - counter
+
+        result = result.ravel()
+        result = result[nonzero(result)]
 
     # Reverse result back if binding is to the end
     if binding == 2:
