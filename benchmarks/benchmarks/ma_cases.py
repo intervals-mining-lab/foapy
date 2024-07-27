9a3698679aca1927713a805dfe619fc766a584ce
Igor Rodionov <goruha@gmail.com>
2024-07-28 02:01:17 +0300

    Update ma_cases.py

--- benchmarks/benchmarks/ma_cases.py
+++ benchmarks/benchmarks/ma_cases.py
@@ -6,6 +6,7 @@ from numpy import fix
 def best_case(length):
     return ma.masked_array(numpy.ones((length,), dtype=int), mask=[0] * length)
 
+
 def dna_case(length):
     rng = numpy.random.default_rng()
     alphabet = numpy.asanyarray(["A", "C", "G", "T"])
@@ -14,6 +15,7 @@ def dna_case(length):
     indecies = rng.integers(0, power, length)
     return ma.masked_array(alphabet[indecies], alphabet_mask[indecies])
 
+
 def normal_case(length):
     rng = numpy.random.default_rng()
     power = int(numpy.fix(length * 0.2))
@@ -21,6 +23,7 @@ def normal_case(length):
     alphabet_mask = numpy.random.choice([True, False], power)
     indecies = rng.integers(0, power, length)
     return ma.masked_array(alphabet[indecies], alphabet_mask[indecies])
+
 
 def worst_case(length):
     seq = numpy.random.rand(length)
