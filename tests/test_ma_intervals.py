c964f16c503c3c1581ee15dd37f7c533bab51c15
Igor Rodionov <goruha@gmail.com>
2024-10-24 02:34:26 +0300

    Fix ma namespaces (#66)

--- tests/test_ma_intervals.py
+++ tests/test_ma_intervals.py
@@ -6,7 +6,7 @@ import pytest
 from numpy.ma.testutils import assert_equal
 
 from foapy.exceptions import InconsistentOrderException, Not1DArrayException
-from foapy.ma.intervals import intervals
+from foapy.ma import intervals
 
 
 class TestMaIntervals(TestCase):
