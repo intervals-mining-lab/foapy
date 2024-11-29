3d23a1d3055f652f1ab3025f0f7ea7ef5d119dce
Maximus2012 <mkochetkov9@gmail.com>
2024-11-29 12:28:00 +0600

    added tests for masked array in depth, rewrote docs for volume and depth

--- src/foapy/characteristics/ma/depth.py
+++ src/foapy/characteristics/ma/depth.py
@@ -6,11 +6,12 @@ from foapy.characteristics.ma.volume import volume
 def depth(intervals):
     """
     Calculation depth of sequence.
-    Depth is the log2 of the volume.
+    Depth is the log2 of the volume, where volume is the
+    product of the elements of the intervals of the sequence .
 
     Parameters
     ----------
-    X: two-dimensional array
+    X: two-dimensional intervals sequence array
         Source array sequence.
 
 
@@ -20,5 +21,64 @@ def depth(intervals):
 
     Examples
     --------
+
+    ----1----
+    >>> X = [
+            [1 4 4]
+            [1 3]
+            [3 1]
+            ]
+    >>> b = depth(X)
+    >>> b
+    [4 1.585 1.585]
+
+    ----2----
+    >>> X = [
+        [1 1 4 4]
+        [3 1 3]
+        [5 3 1]
+    ]
+    >>> b = depth(X)
+    >>> b
+    [4 3.1699 3.9069]
+
+    ----3----
+    >>> X = [
+        [1 4 4 1]
+        [1 3 4]
+        [3 1 2]
+    ]
+    >>> b = depth(X)
+    >>> b
+    [4 3.585 2.585]
+
+    ----4----
+    >>> X = [
+        [4 1 3 3]
+        ]
+    >>> b = depth(X)
+    >>> b
+    [5.1699]
+
+    ----5----
+    >>> X = [[]]
+    >>> b = depth(X)
+    >>> b
+    []
+
+    ----6----
+    >>> X = [[1]]
+    >>> b = depth(X)
+    >>> b
+    [0]
+
+    ----7----
+    >>> X = [
+        [1 1 1 1 1]
+    ]
+    >>> b = depth(X)
+    >>> b
+    [0]
+
     """
     return np.asanyarray([np.log2(line) for line in volume(intervals)])
