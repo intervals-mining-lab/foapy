d44ea610cfdd897759fae2cf5c3ac74cb710f907
masha-mtc <107871776+masha-mtc@users.noreply.github.com>
2025-01-25 17:04:00 +0600

    volume (#49)
    * volume
    
    * volume
    
    * volume new
    
    * volume new
    
    * new
    
    * new
    
    * new
    
    * Added tests for characteristics: volume, depth, geometric mean, average remoteness, arithmetic mean.
    
    * Added tests for characteristics: volume, depth, geometric mean, average remoteness, arithmetic mean.
    
    * Added tests for characteristics: volume, depth, geometric mean, average remoteness, arithmetic mean.
    
    * Change in the implementation of characteristics: entropy, uniformity, descriptive information, regularity.
    
    * Changed accuracy, added inequalities to tests.
    
    * Changed accuracy, added inequalities to tests.
    
    * Changed accuracy, added inequalities to tests.
    
    * Update ci.yml
    
    ---------
    
    Co-authored-by: Igor Rodionov <goruha@gmail.com>

---
+++ src/foapy/characteristics/depth.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from foapy.characteristics.volume import volume
+
+
+def depth(intervals):
+    """
+    Calculation depth of sequence.
+
+    Depth is the log2 of the volume.
+
+    param name = "intervals" (sequence of intervals).
+
+    """
+    return np.log2(volume(intervals))
