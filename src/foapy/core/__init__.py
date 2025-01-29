f688dab28b34488570ad4e674b1f79ee15166211
Igor Rodionov <goruha@gmail.com>
2025-01-30 00:57:21 +0100

    Deploy preview documentation

--- src/foapy/core/__init__.py
+++ src/foapy/core/__init__.py
@@ -10,11 +10,14 @@ except NameError:
 if __FOAPY_SETUP__:
     sys.stderr.write("Running from foapy.core source directory.\n")
 else:
+    # isort: off
     from ._alphabet import alphabet  # noqa: F401
     from ._binding import binding  # noqa: F401
-    from ._intervals import intervals  # noqa: F401
     from ._mode import mode  # noqa: F401
+    from ._intervals import intervals  # noqa: F401
     from ._order import order  # noqa: F401
+
+    # isort: on
 
     __all__ = list({"binding", "mode", "intervals", "order", "alphabet"})
 
