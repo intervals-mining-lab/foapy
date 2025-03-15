a676409adee03cdd8f1697e73cf91248098f2671
Igor Rodionov <goruha@gmail.com>
2025-03-15 18:23:20 +0100

    Refactor tests

--- tests/test_characteristics/test_ma_identifying_information.py
+++ tests/test_characteristics/test_ma_identifying_information.py
@@ -1,205 +1,104 @@
-from unittest import TestCase
-
 import numpy as np
 import numpy.ma as ma
+from test_characteristics.characterisitcs_test import MACharacteristicsTest
 
 from foapy import binding as binding_constant
 from foapy import mode as mode_constant
 from foapy.characteristics.ma import identifying_information
-from foapy.ma import intervals, order
 
 
-class TestMaIdentifyingInformation(TestCase):
+class TestMaIdentifyingInformation(MACharacteristicsTest):
     """
     Test list for identifying information calculate
     """
 
+    epsilon = np.float_power(10, -2)
+
+    def target(self, X, dtype=None):
+        return identifying_information(X)
+
     def test_calculate_start_normal_entropy(self):
         X = ma.masked_array([2, 4, 2, 2, 4])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.normal
-        )
-        expected = np.array([0.415037499, 1.321928094887])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+        expected = [0.415037499, 1.321928094887]
+        self.AssertCase(X, binding_constant.start, mode_constant.normal, expected)
 
-    def test_calculate_start_single_elements_normal_entropy(self):
-        X = ma.masked_array([1, 2, 3])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.normal
-        )
-        expected = np.array([0, 1, 1.5849625])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
-
-    def test_calculate_end_single_elements_normal_entropy(self):
-        X = ma.masked_array([1, 2, 3])
-        order_seq = order(X)
-        intervals_seq = intervals(order_seq, binding_constant.end, mode_constant.normal)
-        expected = np.array([1.5849625, 1, 0])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+    def test_dataset_1(self):
+        X = [1, 2, 3]
+        masked_X = ma.masked_array(X)
+        dtype = np.longdouble
+        expected = {
+            binding_constant.start: {
+                mode_constant.normal: [0, 1, 1.5849625],
+            },
+            binding_constant.end: {
+                mode_constant.normal: [1.5849625, 1, 0],
+            },
+        }
+        self.AssertBatch(masked_X, expected, dtype=dtype)
 
     def test_calculate_end_single_element_lossy_entropy(self):
         X = ma.masked_array(["B"])
-        order_seq = order(X)
-        intervals_seq = intervals(order_seq, binding_constant.end, mode_constant.normal)
-        expected = np.array([0])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+        expected = [0]
+        self.AssertCase(X, binding_constant.end, mode_constant.lossy, expected)
 
     def test_calculate_end_void_lossy_entropy(self):
         X = ma.masked_array([])
-        order_seq = order(X)
-        intervals_seq = intervals(order_seq, binding_constant.end, mode_constant.lossy)
-        expected = np.array([])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+        expected = []
+        self.AssertCase(X, binding_constant.end, mode_constant.lossy, expected)
 
-    def test_calculate_start_lossy(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.lossy
-        )
-        expected = np.array([1.5849, 1, 1])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
-
-    def test_calculate_start_normal(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.normal
-        )
-        expected = np.array([1.3219, 1.22239, 1.5849])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
-
-    def test_calculate_end_normal(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(order_seq, binding_constant.end, mode_constant.normal)
-        expected = np.array([1.3210, 1.4150, 1])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
-
-    def test_calculate_start_redunant(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.redundant
-        )
-        expected = np.array([1.1375, 1.45943, 1.4594])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
-
-    def test_calculate_start_cycle(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.cycle
-        )
-        expected = np.array([1.3219, 1.7369, 1.73696])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+    def test_dataset_2(self):
+        X = ["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"]
+        masked_X = ma.masked_array(X)
+        dtype = np.longdouble
+        expected = {
+            binding_constant.start: {
+                mode_constant.lossy: [1.5849, 1, 1],
+                mode_constant.normal: [1.3219, 1.22239, 1.5849],
+                mode_constant.redundant: [1.1375, 1.45943, 1.4594],
+                mode_constant.cycle: [1.3219, 1.7369, 1.73696],
+            },
+            binding_constant.end: {
+                mode_constant.normal: [1.3210, 1.4150, 1],
+            },
+        }
+        self.AssertBatch(masked_X, expected, dtype=dtype)
 
     def test_calculate_start_lossy_empty_values_uniformity(self):
         X = ma.masked_array([])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.lossy
-        )
-        expected = np.array([0])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+        expected = []
+        self.AssertCase(X, binding_constant.start, mode_constant.lossy, expected)
 
     def test_calculate_start_redunant_values_with_mask(self):
         X = ["B", "B", "B", "A", "A", "B", "B", "A", "B", "B"]
         mask = [1, 1, 1, 0, 0, 1, 1, 0, 1, 1]
         masked_X = ma.masked_array(X, mask)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.redundant
+        expected = [1.4594]
+        self.AssertCase(
+            masked_X, binding_constant.start, mode_constant.redundant, expected
         )
-        expected = np.array([1.4594])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
 
     def test_calulate_normal_with_the_same_values(self):
         X = ["A", "A", "A", "A", "A"]
         masked_X = ma.masked_array(X)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.normal
+        expected = [0]
+        self.AssertCase(
+            masked_X, binding_constant.start, mode_constant.normal, expected
         )
-        expected = np.array([0])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
 
     def test_calculate_start_cycle_with_masked_single_value(self):
         X = ["A"]
         mask = [1]
         masked_X = ma.masked_array(X, mask)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.cycle
-        )
-        expected = np.array([])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+        expected = []
+        self.AssertCase(masked_X, binding_constant.start, mode_constant.cycle, expected)
 
     def test_calculate_start_cycle_with_single_value(self):
         X = ["A"]
         masked_X = ma.masked_array(X)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.cycle
-        )
-        expected = np.array([0])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+        expected = [0]
+        self.AssertCase(masked_X, binding_constant.start, mode_constant.cycle, expected)
 
     def test_calculate_start_lossy_different_values(self):
         X = ma.masked_array(["B", "A", "C", "D"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.lossy
-        )
-        expected = np.array([0, 0, 0, 0])
-        exists = identifying_information(intervals_seq)
-        epsilon = 0.01
-        diff = np.absolute(expected - exists)
-        self.assertTrue(np.all(diff < epsilon))
+        expected = [0, 0, 0, 0]
+        self.AssertCase(X, binding_constant.start, mode_constant.lossy, expected)
--- tests/test_characteristics/test_ma_volume.py
+++ tests/test_characteristics/test_ma_volume.py
@@ -1,130 +1,75 @@
-from unittest import TestCase
-
 import numpy as np
 import numpy.ma as ma
-from numpy.ma.testutils import assert_equal
+from test_characteristics.characterisitcs_test import MACharacteristicsTest
 
 from foapy import binding as binding_constant
 from foapy import mode as mode_constant
 from foapy.characteristics.ma import volume
-from foapy.ma import intervals, order
 
 
-class TestMaVolume(TestCase):
+class TestMaVolume(MACharacteristicsTest):
     """
     Test list for volume calculate
     """
 
-    def test_calculate_start_lossy_volume(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.lossy
-        )
-        expected = np.array([16, 3, 3])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
+    epsilon = np.float_power(10, -4)
 
-    def test_calculate_start_normal_volume(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.normal
-        )
-        expected = np.array([16, 9, 15])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
+    def target(self, X, dtype=None):
+        return volume(X)
 
-    def test_calculate_end_normal_volume(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(order_seq, binding_constant.end, mode_constant.normal)
-        expected = np.array([16, 12, 6])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
-
-    def test_calculate_start_redunant_volume(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.redundant
-        )
-        expected = np.array([16, 36, 30])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
-
-    def test_calculate_start_cycle_volume(self):
-        X = ma.masked_array(["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.cycle
-        )
-        expected = np.array([16, 18, 18])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
+    def test_dataset_1(self):
+        X = ["B", "B", "A", "A", "C", "B", "A", "C", "C", "B"]
+        masked_X = ma.masked_array(X)
+        dtype = np.longdouble
+        expected = {
+            binding_constant.start: {
+                mode_constant.lossy: [16, 3, 3],
+                mode_constant.normal: [16, 9, 15],
+                mode_constant.redundant: [16, 36, 30],
+                mode_constant.cycle: [16, 18, 18],
+            },
+            binding_constant.end: {
+                mode_constant.normal: [16, 12, 6],
+            },
+        }
+        self.AssertBatch(masked_X, expected, dtype=dtype)
 
     def test_calculate_start_lossy_different_values_volume(self):
         X = ma.masked_array(["B", "A", "C", "D"])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.lossy
-        )
-        expected = np.array([1, 1, 1, 1])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
+        expected = [1, 1, 1, 1]
+        self.AssertCase(X, binding_constant.start, mode_constant.lossy, expected)
 
     def test_calculate_start_lossy_empty_values_volume(self):
         X = ma.masked_array([])
-        order_seq = order(X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.lossy
-        )
-        expected = np.array([])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
+        expected = []
+        self.AssertCase(X, binding_constant.start, mode_constant.lossy, expected)
 
     def test_calculate_start_redunant_values_with_mask(self):
         X = ["B", "B", "B", "A", "A", "B", "B", "A", "B", "B"]
         mask = [1, 1, 1, 0, 0, 1, 1, 0, 1, 1]
         masked_X = ma.masked_array(X, mask)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.redundant
+        expected = [36]
+        self.AssertCase(
+            masked_X, binding_constant.start, mode_constant.redundant, expected
         )
-        expected = np.array([36])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
 
     def test_calulate_normal_with_the_same_values(self):
         X = ["A", "A", "A", "A", "A"]
         masked_X = ma.masked_array(X)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.normal
+        expected = [1]
+        self.AssertCase(
+            masked_X, binding_constant.start, mode_constant.normal, expected
         )
-        expected = np.array([1])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
 
     def test_calculate_start_cycle_with_masked_single_value(self):
         X = ["A"]
         mask = [1]
         masked_X = ma.masked_array(X, mask)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.cycle
-        )
-        expected = np.array([])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
+        expected = []
+        self.AssertCase(masked_X, binding_constant.start, mode_constant.cycle, expected)
 
     def test_calculate_start_cycle_with_single_value(self):
         X = ["A"]
         masked_X = ma.masked_array(X)
-        order_seq = order(masked_X)
-        intervals_seq = intervals(
-            order_seq, binding_constant.start, mode_constant.cycle
-        )
-        expected = np.array([1])
-        exists = volume(intervals_seq)
-        assert_equal(expected, exists)
+        expected = [1]
+        self.AssertCase(masked_X, binding_constant.start, mode_constant.cycle, expected)
