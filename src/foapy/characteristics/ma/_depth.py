Fix overflow characterisitcs

src/foapy/characteristics/ma/_depth.py
@@ -59,9 +59,6 @@ def depth(intervals, dtype=None):
     # [4.        3.169925  3.9068906]
     ```
     """  # noqa: W605
-
-    from foapy.characteristics.ma import volume
-
     return np.asanyarray(
-        [np.log2(line, dtype=dtype) for line in volume(intervals, dtype=dtype)]
+        [np.sum(np.log2(line, dtype=dtype), dtype=dtype) for line in intervals]
     )

src/foapy/characteristics/ma/_geometric_mean.py
@@ -61,11 +61,14 @@ def geometric_mean(intervals, dtype=None):
     # [2.         2.08008382 2.46621207]
     ```
     """  # noqa: W605
-
     return np.asanyarray(
         [
             (
-                np.float_power(np.prod(line, dtype=dtype), 1 / len(line), dtype=dtype)
+                np.power(
+                    2,
+                    np.sum(np.log2(line, dtype=dtype), dtype=dtype) / len(line),
+                    dtype=dtype,
+                )  # noqa: E501 E261
                 if len(line) != 0
                 else 0
             )

tests/test_characteristics/test_ma_geometric_mean.py
@@ -13,7 +13,7 @@ class TestGeometricMean(MACharacteristicsTest):
     Test list for geometric_mean calculate
     """
 
-    epsilon = np.float_power(10, -100)
+    epsilon = np.float_power(10, -15)
 
     def target(self, X, dtype=None):
         return geometric_mean(X)

tests/test_characteristics/test_ma_periodicity.py
@@ -12,7 +12,7 @@ class TestMaPeriodicity(MACharacteristicsTest):
     Test list for uniformity calculate
     """
 
-    epsilon = np.float_power(10, -100)
+    epsilon = np.float_power(10, -15)
 
     def target(self, X, dtype=None):
         return periodicity(X)